5156db4a51351e5e22c530c5a6e60768a2f42b1b
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-16 18:56:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push(arr, val)
+  arr << val
+end
+
+def using_unshift(arr,val)
+  arr.unshift(val)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
