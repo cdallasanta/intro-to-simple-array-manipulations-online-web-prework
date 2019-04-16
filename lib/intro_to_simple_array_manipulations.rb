99c108f12d594daee54b3d77dc87395f589dcefe
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-16 18:57:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,3 +19,12 @@ def using_shift(arr)
 end
 
 def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr1, elem)
+  arr1.insert(elem)
